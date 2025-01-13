ef0e64b6b0cfe90c0bafe18c08619b4f06a21f26
ZincOdu <473092432@qq.com>
2025-01-13 16:29:28 +0800

    仅输出最后一个数

--- smooth.c
+++ smooth.c
@@ -4,10 +4,46 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAX(a, b) ((a) > (b)? (a) : (b))
+#define MIN(a, b) ((a) < (b)? (a) : (b))
+
 // smooth子函数: 高斯函数
 float gaussian(float x, float sigma) {
 	return exp(-0.5f * (x / sigma) * (x / sigma));
 }
+
+
+// 双边滤波仅修正最后一个窗口的数据 空间域用左半个高斯滤波核
+// 返回的filtered_data是一个长度为5的数组（半个窗口大小）
+void bilateral_filter_1d_last(float* data, int length, float sigma_d, float sigma_r, float* filtered_data) {
+	int half_window = 5; //半窗口大小取固定值5 与异常值剔除函数outlier_filter_last的窗口大小对应
+
+	for (int i = 0; i < half_window; i++) {
+		float sum_weights = 0.0f;
+		float sum_weighted_values = 0.0f;
+		int data_idx = length - half_window + i;
+
+		for (int j = -half_window; j <= half_window; j++)
+		{
+			int index = data_idx + j;
+			if (index >= 0 && index < length) {
+				// 空间域权重
+				float spatial_weight = gaussian(j, sigma_d);
+				// 值域权重
+				float range_weight = gaussian(data[index] - data[data_idx], sigma_r);
+				// 总权重
+				float weight = spatial_weight * range_weight;
+			
+				sum_weights += weight;
+				sum_weighted_values += weight * data[index];
+			}
+		}
+
+		filtered_data[i] = sum_weighted_values / sum_weights;
+	}
+
+}
+
 
 // smooth子函数: 双边滤波
 void bilateral_filter_1d(float* data, int length, float sigma_d, float sigma_r, float* filtered_data) {
@@ -102,6 +138,29 @@ void outlier_filter(float data[], int n) {
 
 }
 
+// smooth_last子函数 iqr方法判断最优一个数是否为异常值
+// 若不是则返回原始值 若是则用最邻近值替换
+// n需大于等于5
+float outlier_filter_last(float data[], int n) {
+	float window[5];
+	
+	for (int i = 0; i < 5; i++){
+		window[i] = data[n-1-i];
+	}
+	
+	float res = window[0];
+	float near = window[1];
+	
+	// iqr方法判断是否为异常值
+	quick_sort(window, 0, 4);
+	float th_iqr = (window[3] - window[1]) * 1.5;
+	float th = MAX(window[4] * 0.5, th_iqr);
+	if (res < window[1] - th || res > window[3] + th){
+		res = near;
+	}
+
+	return res;
+}
 
 // 数据平滑函数
 // data: 待平滑数据地址
@@ -109,8 +168,19 @@ void outlier_filter(float data[], int n) {
 // smoothed_data: 平滑后数据存放的地址
 void smooth(float* data, int length, float* smoothed_data) {
 	
-	bilateral_filter_1d(data, length, 10.0, 1200.0, smoothed_data);
+	bilateral_filter_1d(data, length, 3.0, 30.0, smoothed_data);
 	outlier_filter(smoothed_data, length);
+}
+
+//数据平滑 仅输出最后一个数据
+// data: 待平滑数据地址
+// length: 待平滑数据长度 需大于等于5 取值为11则效果最佳
+float smooth_last(float* data, int length) {
+
+	float filtered_data[5] = {0.f, 0.f, 0.f, 0.f, 0.f};
+	bilateral_filter_1d_last(data, length, 3.0, 30.0, filtered_data);
+	return outlier_filter_last(filtered_data, 5);
+
 }
 
 // 使用示例1
@@ -118,8 +188,9 @@ void smooth(float* data, int length, float* smoothed_data) {
 int main() {
 	
 	// 原始数据
-	float data[] = {1100, 966, 476, 1132, 1270, 853, 464, 993, 1276, 1223,
-		891, 499, 1273, 1245, 841, 485, 1214, 1214, 1000, 372, 1112};
+	float data[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+		2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+		3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
 	int length = sizeof(data) / sizeof(data[0]);
 	
 	// 待平滑数据申请内存
@@ -131,16 +202,15 @@ int main() {
 	// 结果打印
 	printf("原始数据: ");
 	for (int i = 0; i < length; i++) {
-		printf("%f ", data[i]);
+		printf("%f,", data[i]);
 	}
 	printf("\n");
 
 	printf("平滑数据: ");
 	for (int i = 0; i < length; i++) {
-		printf("%f ", smoothed_data[i]);
+		printf("%f,", smoothed_data[i]);
 	}
 	printf("\n");
-
 
 	// 内存释放
 	free(smoothed_data);
@@ -149,7 +219,7 @@ int main() {
 #endif
 
 // 使用示例2: 读一个文件中的数据做平滑操作 平滑后的数据写到另一个文件里
-#if 1
+#if 0
 
 #define MAX_LINES 4000  // 文件最大行数
 
@@ -202,12 +272,12 @@ int main() {
 		int sm_data_length = end - start;
 		smooth(data + start, sm_data_length, smoothed_data);
 		
-		// 平滑结果追加写到文件
-		file = fopen(filename_out, "a");
-		for (int i = 0; i < sm_data_length; i++) {
-			fprintf(file, "%f\n", smoothed_data[i]);
-		}
-		fclose(file);
+		//// 平滑结果追加写到文件
+		//file = fopen(filename_out, "a");
+		//for (int i = 0; i < sm_data_length; i++) {
+		//	fprintf(file, "%f\n", smoothed_data[i]);
+		//}
+		//fclose(file);
 
 		start += batch_sz;
 	}
@@ -220,5 +290,176 @@ int main() {
 #endif
 
 
+// 使用示例3: ic、tc数据
+#if 0
 
+#define MAX_LINES 500  // 文件最大行数
 
+int read_file_to_array(const char *filename, float *array, int max_lines) {
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		perror("Error opening file");
+		return -1;
+	}
+
+	int count = 0;
+	while (count < max_lines && fscanf(file, "%f", &array[count]) == 1) {
+		count++;
+	}
+
+	fclose(file);
+	return count;
+}
+
+int main() {
+
+	// 输入数据文件
+	const char *filename = "./tc.txt";
+
+	// 输出文件
+	const char *filename_out = "./tc_smoothed.txt";
+	FILE *file = fopen(filename_out, "w");
+	fclose(file);
+
+	// 读取数据
+	float data[MAX_LINES];
+	int num_values = read_file_to_array(filename, data, MAX_LINES);
+
+	// 待平滑数据的长度 每sm_data_length个数据处理一次
+	int batch_sz = 15;
+	int step = 1;
+
+	// 待平滑数据申请内存
+	float* smoothed_data = (float*)malloc(batch_sz * sizeof(float));
+
+	// 开始处理
+	int start = 0;
+	while (start < num_values) {
+
+		int end = start + batch_sz;
+
+		if (end > num_values) {
+			end = num_values;
+		}
+		// 平滑
+		int sm_data_length = end - start;
+		smooth(data + start, sm_data_length, smoothed_data);
+
+		// 平滑结果追加写到文件
+		file = fopen(filename_out, "a");
+		fprintf(file, "%f\n", smoothed_data[0]);
+		fclose(file);
+
+		start += step;
+	}
+
+	// 内存释放
+	free(smoothed_data);
+	return 0;
+}
+
+#endif
+
+// 使用示例4
+# if 0
+int main() {
+
+	// 原始数据
+	float data[] = { 571.9,	567.6,	570.9,	574.9,	565.4,	571.4,	567.7,	563.7,	566.5,	564.9,	566.6,	566.3,	565.9,	565.8,	566.7,	567.5,	567,	566.1,	564.4,	562.2,	561,	560.6,	560,	559.2,	558.8,	557.2,	534.1,	291.3,	70.5,	70.1,	70.1,	69.7,	69.3,	69.2,	69.1,	69.1,	68.7,	68.1,	67.6,	67.2,	67.1,	67.2,	67.6,	67.9,	67.5,	67,	66.5,	66.2,	66,	65.6,	65.3,	65.1,	65.1,	65.3,	65.7,	66.4,	67.2,	68.2,	69.1,	69.4,	69.7,	68.8,	67.8,	66.7,	65.9,	65.3,	65.2,	64.8,	64.4,	64.2,	64.1,	64.2,	64.5,	65,	65.8,	66.8,	67.8,	69.6,	68.9,	66.8,	63.6,	61.2,	59.4,	58.1,	57.8,	57.6,	57.7,	58,	58.6,	59.1,	59,	59,	59,	58.9,	59.1,	59,	59,	59.1,	59.1,	58.8,	58.7,	58.7,	58.7,	59,	59.3,	59.2,	59,	59.1,	59.6,	59.5,	59.2,	59.1,	59.2,	59.2,	58.9,	59.1,	59.2,	59.3,	59,	58.8,	59,	59.2,	59.2,	59,	58.8,	59,	59.3,	59.3,	59.1,	59.1,	59.3,	59.4,	59.2,	59,	58.9,	58.8,	58.7,	58.7,	58.5,	58.5,	58.6,	59.1,	58.9,	58.7,	58.8,	58.9, };
+	int length = sizeof(data) / sizeof(data[0]);
+
+	// 待平滑数据的长度 每sm_data_length个数据处理一次
+	int batch_sz = 12;
+	int step = 1;
+
+	// 待平滑数据申请内存
+	float* smoothed_data = (float*)malloc(batch_sz * sizeof(float));
+
+	// 开始处理
+	int start = 0;
+	while (start < length) {
+
+		int end = start + batch_sz;
+
+		if (end > length) {
+			end = length;
+		}
+		// 平滑
+		int sm_data_length = end - start;
+		smooth(data + start, sm_data_length, smoothed_data);
+
+		printf("%f,", smoothed_data[5]);
+
+		start += step;
+	}
+
+	// 内存释放
+	free(smoothed_data);
+	return 0;
+}
+#endif
+
+// 测试
+# if 0
+int main() {
+
+	float data[11] = {567, 566, 564, 562, 561, 560, 558, 557, 534, 291, 70};
+
+	printf("%f", smooth_last(data, 11));
+	return 0;
+}
+#endif
+
+// 测试 读文件
+#if 1
+
+#define MAX_LINES 4000  // 文件最大行数
+
+int read_file_to_array(const char *filename, float *array, int max_lines) {
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		perror("Error opening file");
+		return -1;
+	}
+
+	int count = 0;
+	while (count < max_lines && fscanf(file, "%f", &array[count]) == 1) {
+		count++;
+	}
+
+	fclose(file);
+	return count;
+}
+
+int main() {
+
+	// 输入数据文件
+	const char *filename = "./dat3.txt";
+
+	// 读取数据
+	float data[MAX_LINES];
+	int num_values = read_file_to_array(filename, data, MAX_LINES);
+
+	float min_outlier_th = 300.f;
+
+	// 开始处理
+	int start = 0;
+	while (start < num_values) {
+
+		int end = start + 11;
+
+		if (end > num_values) {
+			return 0;
+		}
+
+		float smoothed = smooth_last(data + start, 11, min_outlier_th);
+		float raw = data[start + 10];
+		printf("%f,%f\n", raw, smoothed);
+		
+		start += 1;
+	}
+
+	return 0;
+}
+
+#endif
